c4f89a7ad6a1e7119b4f84e7b73e4059b0473f64
Sabhya <sabgrohyaver@gmail.com>
2020-06-30 12:43:44 +0530

    changed contents of App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is Sabhya's App</Text>
       <StatusBar style="auto" />
     </View>
   );
